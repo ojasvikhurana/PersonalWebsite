deccb3adf6a2d7db92f134a2743a86e4cb468357
ojasvik <khuranaojasvi@gmail.com>
2020-07-20 22:44:23 +0530

    FirstPage

---
+++ index.js
@@ -0,0 +1,12 @@
+let i = 0;
+let txt = 'I am an engineering undergrad from India who loves to technology and communities.'
+let speed = 50;
+function typewriter(){
+    if(i<txt.length){
+        document.getElementById("info").innerHTML+=txt.charAt(i);
+        i++;
+        setTimeout(typewriter,speed);
+    }
+};
+
+window.onload = typewriter();
